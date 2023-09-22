0d8c62cec11ded5d074421cc10e825874a3009d0
LWJ <leongwj-wm20@student.tarc.edu.my>
2023-09-22 11:31:50 +0800

    Updates 1. Add selection path 2. Add recycle list

---
+++ app/src/main/java/com/example/greenwizard/fragments/list/ListRecycleFragment.kt
@@ -0,0 +1,31 @@
+package com.example.greenwizard.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.greenwizard.R
+import com.example.greenwizard.viewmodel.LocationViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class listRecycle : Fragment() {
+
+    private lateinit var mLocationViewModel: LocationViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list_recycle, container, false)
+
+        return view
+    }
+}
